21f48c16954e5ea60815798a68b4d8e5fbadde35
Cavon <cavonxx@gmail.com>
2026-05-08 13:48:12 +0800

    ci: update workflow to build from installer-scripts, add umbrel app manifest

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-CONFIG_PATH="${CONFIG_PATH:-/data/config.json}"
-mkdir -p "$(dirname "$CONFIG_PATH")"
-
-exec /opt/pironman5/venv/bin/pironman5 --config-path "$CONFIG_PATH" start
